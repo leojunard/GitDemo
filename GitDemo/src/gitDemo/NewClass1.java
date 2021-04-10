3f5433591049d6a0d86c3c68c3addabc010dadea
leojunard <leonardo.tajantajanjr@gmail.com>
2021-04-11 01:56:23 +0800

    New Codes

---
+++ GitDemo/src/gitDemo/NewClass1.java
@@ -0,0 +1,11 @@
+package gitDemo;
+
+public class NewClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("NewClass1");
+
+	}
+
+}
---
+++ GitDemo/src/gitDemo/NewClass2.java
@@ -0,0 +1,10 @@
+package gitDemo;
+
+public class NewClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("NewClass2");
+	}
+
+}
